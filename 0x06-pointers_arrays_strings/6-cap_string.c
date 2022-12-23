93ae0be5f2b185f6bcb75d1f734fc5d248b57352
diplomat-7 <nsikan0000@gmail.com>
2022-12-23 15:46:12 -0800

    tttt

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,30 @@
 #include "main.h"
+
 /**
-*cap_string - function that capitalize first character of a word
-*@str: string to capitalize
-*Return:returns the capitalized string
-*/
+* *cap_string - a function that capitalizes all words of a string
+* @str: string
+* Return: string
+**/
+
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i = 1;
 
-	while (str[++index])
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+
+	while (str[i] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t'
+		     || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+		     || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
+		     || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
+		     || str[i - 1] == '{' || str[i - 1] == '}')
+		     && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
 	}
 	return (str);
 }
