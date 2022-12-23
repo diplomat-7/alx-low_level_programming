1acd84e4fa02d2675ee080f80e132337a4ada23d
diplomat-7 <nsikan0000@gmail.com>
2022-12-23 15:22:38 -0800

    lorem

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,39 @@
-#include <stdio.h>
+#include "holberton.h"
 
-int main(void)
+/**
+ * print_number - print integers
+ * @n: integer value.
+ *
+ * Return: nothing
+ */
+void print_number(int n)
 {
-  int n;
-  int a[5];
-  int *p;
+	unsigned int a, b, count, tmp, pow;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	a = n;
+	pow = b = 1;
+
+	if (n < 0)
+	{
+		a *= -1;
+		_putchar('-');
+	}
+
+	tmp = a;
+
+	while (tmp > 9)
+	{
+		b++;
+		tmp /= 10;
+	}
+
+	for (count = 1; count < b; count++)
+		pow *= 10;
+
+	while (pow > 1)
+	{
+		_putchar((a / pow) % 10 + '0');
+		pow = pow / 10;
+	}
+	_putchar(a % 10 + '0');
 }
